2eb7857a0c0d57ba78e400832c5b6fc691e65e7e
imdhruvkumar <dk0495682@gmail.com>
2026-08-21 11:14:36 +0530

    py installation and print hello.

--- 01_Basics/hello.py
+++ 01_Basics/hello.py
@@ -1,1 +1,11 @@
+
+# this is simple hello print||
+
 print("Hello, World!")
+
+# this is method definatioin in python
+
+def greet(n):
+    print(n)
+
+greet("dhruv")
---
+++ 01_Basics/method.py
@@ -0,0 +1,6 @@
+
+#this how a file import in anoter file
+
+from hello import greet
+
+greet("dhruv kumar")
