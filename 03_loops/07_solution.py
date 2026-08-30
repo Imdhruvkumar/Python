a7685954a9d2913732448920b40f3da6e2c8175d
imdhruvkumar <dk0495682@gmail.com>
2026-08-30 17:09:41 +0530

    loops in python

---
+++ 03_loops/07_solution.py
@@ -0,0 +1,10 @@
+while True:
+    number = input("Enter a number b/w 0 to 1 :\n")
+    input_number = int(number)
+    if 1 <= input_number <=10:
+        print(" number is valid")
+        break
+    else:
+        print("invalid no.")
+
+
