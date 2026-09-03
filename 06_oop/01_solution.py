a7ad0b876779409fe201ce538858b60e1773b07d
imdhruvkumar <dk0495682@gmail.com>
2026-09-03 12:02:24 +0530

    functions in python part 2

--- 06_oop/01_solution.py
+++ 06_oop/01_solution.py
@@ -3,14 +3,14 @@ class Car:
 
     def __init__(self, brand, model):
         self.__brand = brand
-        self.model = model
+        self.__model = model
         Car.total_car +=1
 
     def get_brand(self):
         return self.__brand
 
     def fullname(self):
-        return f"{self.__brand}{self.model}"
+        return f"{self.__brand}{self.__model}"
 
     def fuel_type(self):
         return "petrol or diesal"
@@ -18,6 +18,13 @@ class Car:
     @staticmethod
     def general_descrption():
         return "car is goos with black color"
+
+    @property
+    
+    def model(self):
+        return self.__model
+
+    
 
 
 
@@ -35,6 +42,10 @@ class Electric_car(Car):
 my_car = Car("toyoto","corola")
 new_car = Electric_car("tesla","model","86ksd")
 
+
+# print(isinstance(my_car,Car))
+# print(isinstance(my_car,Electric_car))
+
 # print(new_car.__brand)
 # print(new_car.model)
 # print(new_car.b_size)
@@ -49,8 +60,33 @@ new_car = Electric_car("tesla","model","86ksd")
 # print(Car.total_car)
 # print(my_car.general_descrption())
 # print(Car.general_descrption())
+# my_car.model = "safari"
+# print(my_car.model)
+
 
 
 # my_new_car = Car("fararri","cruse")
 # print(my_new_car.brand)
 # print(my_new_car.model)
+
+
+class Battry:
+    def battry_info(self):
+        return "battry"
+
+class Engine:
+    def engine_info(self):
+        return "engine"
+
+class Electriccar2(Battry,Engine,Car):
+    pass
+
+
+my_incar = Electriccar2("tesla","model")
+
+# print(my_incar.battry_info())
+# print(my_incar.engine_info())
+# print(my_incar.get_brand())
+# print(my_incar.model)
+
+
