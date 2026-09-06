c802c219554550d234f5434b5b1ddba5baa9b00c
imdhruvkumar <dk0495682@gmail.com>
2026-09-06 12:45:07 +0530

    Adding bussiness data for numpy operations

---
+++ 08_numpy/phase_3.py
@@ -0,0 +1,12 @@
+##===========Advance operation  with business example========
+
+#---Data structure:[restaurent id,2020,2021,2022,2023,2024]
+
+import numpy as np
+data = np.arraty([
+    [1, 40000, 50000, 60000, 70000, 80000],# "Biryani"
+    [2, 30000, 40000, 50000, 60000, 70000],# "Pizza"
+    [3, 20000, 30000, 40000, 50000, 60000],# "Dosa"
+    [4, 25000, 35000, 45000, 55000, 65000],# "Burger"
+    [5, 35000, 45000, 55000, 65000, 75000] # "Paneer Tikka"
+])
